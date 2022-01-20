1e285e198f67a459ccb975fd56f778dbec62ddc5
vgj24 <vrushlijoshi24@gmail.com>
2022-01-20 11:16:23 +0530

    line comparison

---
+++ LineComprisonProgram/Linecomparison.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProgram
+{
+    internal class Linecomparison
+    {
+         int x1,y1,x2,y2;
+
+        public Linecomparison(int x1,int y1,int x2,int y2)
+        {
+            this.x1 = x1;
+            this.y1 = y1;   
+            this.x2 = x2;
+            this.y2 = y2;
+            Console.WriteLine("line coordinates are :(x1,y1) =({0},{1}) \n (x2,y2)=({2},{3})",this.x1,this.y1,this.x2,this.y2);
+        }
+
+        public double findlength()
+        {
+            double length=Math.Sqrt( (this.x2 - this.x1)^2 + (this.y2 - this.y1)^2 );
+            Console.WriteLine("length of line is :" + length);
+            return length;
+
+        }
+    }
+}
---
+++ LineComprisonProgram/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LineComparisonProgram;
+
+Console.WriteLine("Line Comparison");
+#region Line comparison
+Linecomparison ln1 = new Linecomparison(12, 5, 14, 5);
+Linecomparison ln2 = new Linecomparison(10, 5, 14, 5);
+double l1 = ln1.findlength();
+double l2 = ln2.findlength();
+
+if (l1.CompareTo(l2) > 0)
+{
+    Console.WriteLine("line one with length ={0} is greater", l1);
+
+}
+else
+{
+    Console.WriteLine("line one with length ={0} is greater", l2);
+}
+#endregion
